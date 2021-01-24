35e8cb35785eb315c72a6b313f5da1c04029a82e
Jonathan Canevese (M1) <jonathan.canevese@develondigital.com>
2021-01-24 22:25:04 +0100

    expo-updates

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,10 +1,31 @@
-import React, { useRef, useCallback } from 'react';
-import { View } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Alert } from 'react-native';
 import LottieView from 'lottie-react-native';
 import Colors from '../constants/Colors';
+import * as Updates from 'expo-updates';
 
 const SplashScreen = ({ navigation, route }) => {
-
+    useEffect(() => {
+        setupUpdates()
+    }, []);
+    const setupUpdates = async () => {
+        Updates.addListener(({ type }) => {
+            if (type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
+                Alert.alert(
+                    'È disponibile una nuova versione',
+                    `Per un'esperienza migliore, è necessario riavviare l'app`,
+                    [
+                        {
+                            text: 'Ok', onPress: async () => {
+                                await Updates.fetchUpdateAsync();
+                                await Updates.reloadAsync();
+                            }
+                        }
+                    ]
+                )
+            }
+        })
+    }
     return (
         <View style={{ flex: 1, backgroundColor: Colors.light.ARANCIO }}>
             <LottieView
